d2125232e1d9a98b8908c376e0e882928eaf7c62
sharmilabanucs24-maker <sharmilabanucs24@gmail.com>
2025-12-15 13:24:38 +0530

    first time installation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,177 @@
-import React from "react";
-class App extends React.Component
+function App()
+{
+  if(true)
+  {
+    var v=100
+    let l=200
+    const c=300
+    v++;
+    l++;
+    console.log("let:"+1);
+  
+  }
+  return(
+    <>
+    <h1>variable : global : {v}</h1>
+    <h1>does not work let and come : because local</h1>
+    </>
+  )
+}
+export default App
+
+
+
+/*function App()
+
+
+
+{
+  let myfun=(a,b,c)=>{
+    var tot=a+b+c
+    document.getElementById("res").innerHTML=`<h2>total:${tot}</h2>`;
+  }
+return(<>
+  <h2>Arrow function withparameter</h2>
+  <button onClick={()=>myfun(10,20,30)}>click me</button>
+  <div id="res"></div>
+</>)
+}
+export default App
+
+/*function App()
+{
+let myfun=()=>{
+    document.getElementById("res").innerHTML="congrats you clicked on the button";
+  }
+return(<>
+  <h2>Arrow function without parameter</h2>
+  <button onClick={myfun}>click me</button>
+  <div id="res"></div>
+
+
+</>)
+
+export default App
+
+
+/*import myimg from './images/5.jpg'
+function App()
+{
+  return(<>
+  <h2>image demo using path as expression</h2>
+  <img src={myimg} width="50%" height="auto" alt="path is wrong"></img>
+  
+  
+  
+  </>
+
+
+
+  )
+}
+export default App
+
+
+
+
+
+
+/*function App()
+{
+  var a=300
+  var b=200
+  var c=400
+  return(
+    <>
+    <h1>biggest amoung 3 no's </h1>
+    <h1>biggest number a or b :{a>b?a:b}</h1>
+    <h1>biggest number a or b or c:{(a>b && a>c) ?a:(b>c?b:c)}</h1>
+
+    </>
+
+  )
+}
+export default App
+
+
+
+
+
+
+
+
+/*function App()
+{
+  var a=100
+  var b=200
+  return(
+    <>
+    <h2>biggest among 2 no's using conditional or ternary operator</h2>
+    <h1>biggest number:{a>b?a:b}</h1>
+    </>
+
+  )
+}
+export default App
+
+
+
+
+
+/*function App()
+
+
+{
+  var a=10
+  var b=20
+  var c="sharmila"
+  return(
+    <>
+    <h1>total:{a+b}</h1>
+    <marquee bgcolor='lightgreen'><font size='7'>{c}</font></marquee>
+  </>)
+
+}
+
+
+
+export default App
+
+
+
+/*import React from "react";
+class Parent extends React.Component
 {
   render()
     {
       return(
         <>
        
-        <h1>welcome to components</h1>
-         <h1>welcome to components</h1>
-          <h1>welcome to components</h1>
-          <p>Nostrud fugiat est eiusmod sunt velit labore enim qui minim duis exercitation Lorem commodo. Cillum esse aliqua cillum esse eiusmod ad do fugiat pariatur amet deserunt elit. Veniam labore aliquip laborum occaecat minim officia quis ullamco ut eiusmod consectetur. Aliquip esse irure nulla deserunt ad eu pariatur sit cillum minim commodo occaecat elit. Ullamco ullamco deserunt laboris ex nulla duis adipisicing non sit esse consequat ut do nisi.Ullamco dolor qui commodo proident id fugiat dolore magna cupidatat cillum irure ea enim. Dolor reprehenderit tempor ad sunt et nostrud exercitation non anim. Do aliqua aute cupidatat culpa minim. Ea dolore laboris reprehenderit ad ad proident non nisi duis irure et ullamco pariatur cillum. Dolor minim est occaecat in cupidatat enim qui ad quis laboris sit. Ad labore pariatur esse adipisicing deserunt aliquip. Ipsum qui nulla incididunt do fugiat ut esse incididunt nulla consequat.
-
-Ut veniam excepteur aute consequat deserunt voluptate. Minim sunt nostrud officia dolore. Culpa do nostrud elit voluptate.
-
-Commodo velit in veniam ullamco cupidatat tempor aliquip. Excepteur velit ea ea non consectetur adipisicing exercitation sit cillum Lorem. Dolor qui amet ipsum sit.</p>
-        </>
-      )
-    
-
+        <h1>this is</h1>
+         <h1>this is parent class</h1>
+          <h1>this is parent class</h1>
+</>)
     }
-  
-}export default App
+  }
+
+class App extends React.Component
+{
+  render()
+  {
+    return(
+      <>
+      <Parent/>
+      <h1>welcome to js</h1>
+      <h1>using react js</h1>
+</>
+    )
+  }
+}
+         
+export default App*/
+
+
+
 
 
 
@@ -32,7 +183,8 @@ Commodo velit in veniam ullamco cupidatat tempor aliquip. Excepteur velit ea ea 
       <h1>welcome to react js</h1>
       </div>
   )
-                             
+     
+  
 }
 export default App*/
 
